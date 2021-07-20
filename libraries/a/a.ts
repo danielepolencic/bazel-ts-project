ce82dc91387ef518dedf215a1d6becc40dc05216
Daniele Polencic <daniele.polencic@gmail.com>
2021-07-20 15:29:42 +0800

    testing types

--- libraries/a/a.ts
+++ libraries/a/a.ts
@@ -1,3 +1,8 @@
 import { b } from "@libs/b";
+import * as Hast from "hast";
+
+let d: Hast.Element | undefined;
+
+console.log(d ?? '')
 
 export const a = `a${b}`;
---
+++ types/hast.d.ts
@@ -0,0 +1,14 @@
+declare module '~hast/index' {
+
+  export interface Element {
+    type: string
+    tagName: string
+    properties: Record<string, unknown>
+  }
+
+}
+
+declare module 'hast' {
+  import alias = require('~hast/index')
+  export = alias
+}
